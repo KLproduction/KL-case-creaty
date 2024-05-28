508d8353f9c2814e5568e08f0c4c3a3a6798f751
KentLaw <kent1988hk@gmail>
2024-05-28 16:18:55 +0100

    finish landing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,15 @@ import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 const Navbar = async () => {
   const {getUser}=getKindeServerSession();
   const user = await getUser();
-
   const isAdmin = user?.email === process.env.ADMIN_EMAIL
+
+  
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-          <Link href='/' className='flex z-40 font-semibold'>
+        <div className='flex h-14 items-center justify-between'>
+          <Link href='/' className='flex z-40 font-bold'>
             case<span className='text-green-600'>cobra</span>
           </Link>
 
@@ -30,7 +31,7 @@ const Navbar = async () => {
                   })}>
                   Sign out
                 </Link>
-                {isAdmin ? (
+                {isAdmin && (
                   <Link
                     href='/dashboard'
                     className={buttonVariants({
@@ -39,7 +40,10 @@ const Navbar = async () => {
                     })}>
                     Dashboard
                   </Link>
-                ) : null}
+                )}
+
+                <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
+
                 <Link
                   href='/configure/upload'
                   className={buttonVariants({
