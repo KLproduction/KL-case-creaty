7129c2bda2499f6711cd43e009228ce66b2285ea
KentLaw <kent1988hk@gmail>
2024-06-02 17:21:31 +0100

    upload fixed

--- src/app/api/uploadthing/core.tsx
+++ src/app/api/uploadthing/core.tsx
@@ -10,40 +10,44 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
+      // Middleware simply forwards the input, ensuring it conforms to the expected schema
       return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input;
-
-      const res = await fetch(file.url);
-      const buffer = await res.arrayBuffer();
-
-      // Using Jimp to read the image and get metadata
-      const image = await Jimp.read(Buffer.from(buffer));
-      const width = image.bitmap.width;
-      const height = image.bitmap.height;
-
-      if (!configId) {
-        const configuration = await db.configuration.create({
-          data: {
-            imageUrl: file.url,
-            height: height || 500,
-            width: width || 500,
-          },
+      try {
+        const { configId } = metadata.input;
+        const res = await fetch(file.url);
+        const buffer = await res.arrayBuffer();
+        const image = await Jimp.read(Buffer.from(buffer));
+        const width = image.bitmap.width;
+        const height = image.bitmap.height;
+    
+        if (!configId) {
+          const configuration = await db.configuration.create({
+            data: {
+              imageUrl: file.url,
+              height: height || 500,
+              width: width || 500,
+            },
+          });
+          return { configId: configuration.id };
+        } else {
+          const updateConfiguration = await db.configuration.update({
+            where: { id: configId },
+            data: { croppedImageUrl: file.url },
+          });
+          return { configId: updateConfiguration.id };
+        }
+      } catch (error) {
+        console.error("Error in onUploadComplete:", error);
+        throw new UploadThingError({
+          message: "Error processing uploaded image",
+          code: "INTERNAL_SERVER_ERROR",
+          cause: error?.toString(),
         });
-        return { configId: configuration.id };
-      } else {
-        const updateConfiguration = await db.configuration.update({
-          where: {
-            id: configId,
-          },
-          data: {
-            croppedImageUrl: file.url,
-          },
-        });
-        return { configId: updateConfiguration.id };
       }
-    }),
+    })
+    
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
